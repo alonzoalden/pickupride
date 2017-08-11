c41cb033bd7635b02f67db76e8eba78d29b43be8
Alonzo Alden <alonzoalden@gmail.com>
2017-08-11 16:05:53 -0700

    Created Nav Bar. Change from SASS -> SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { MdTabsModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdTabsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
