5c6ccd5920d69479c6948d7725f3f173a0d1f51d
Alonzo Alden <alonzoalden@gmail.com>
2017-08-15 16:06:32 -0700

    Created Node server

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var session = require('express-session');
+var methods = require('methods');
+
+var app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(require('method-override')());
+
+app.use(express.static(__dirname + '/dist'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(3000, function () {
+    console.log('DEV server listening on port 3000!');
+});
+
+module.exports = app;
