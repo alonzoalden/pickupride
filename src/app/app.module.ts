9889f9651d4516a86392fb15ee661cb834c94133
Alonzo Alden <alonzoalden@gmail.com>
2017-08-14 20:18:44 -0700

    New Mockup Lead Ride. New Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { MdTabsModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RideComponent } from './discover/ride/ride.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RideComponent,
+    DiscoverComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MdTabsModule,
+    MdSidenavModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/discover/ride/ride.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ride',
+  templateUrl: './ride.component.html'
+})
+export class RideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('balls')
+  }
+
+}
